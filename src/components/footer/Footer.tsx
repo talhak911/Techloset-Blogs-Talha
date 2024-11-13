c92ded3bb5f7057b8c288d3923b3eff255cad31f
Talha Sadiq <talhak911>
2024-11-13 18:39:08 +0500

    chore: change styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 
 function Footer() {
   return (
-    <footer className="flex h-[54px] w-full bottom-0 absolute items-center  bg-stone-200 px-[30px] py-[15px] text-white md:px-[50px] md:py-[15px] lg:h-[100px] lg:py-[34px]">
-    <h3 className="text-slate-950 text-center font-bold text-xl">Follow for more</h3>
-  </footer>
-  )
+    <footer className="flex h-[54px] w-full items-center bg-stone-200 px-[30px] py-[15px] text-white md:px-[50px] md:py-[15px] lg:h-[60px] lg:py-[34px]">
+      <h3 className="text-center text-xl font-bold text-slate-950">
+        Follow for more
+      </h3>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 
 function Header() {
   return (
-    <nav className="flex h-[54px] top-0 mt-3 sticky items-center justify-between bg-stone-200 px-[30px] py-[15px] text-white md:px-[50px] md:py-[15px] lg:h-[100px] lg:py-[34px]">
-      <h1 className="text-slate-950 font-bold text-2xl">Techloset Blogs Site</h1>
+    <nav className="flex h-[54px] items-center justify-between bg-stone-200 px-[30px] py-[15px] text-white md:px-[50px] md:py-[15px] lg:h-[60px] lg:py-[34px]">
+      <h1 className="text-2xl font-bold text-slate-950">
+        Techloset Blogs Site
+      </h1>
     </nav>
   );
 }
